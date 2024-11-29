02ac512772248cdb9cc6c414028bf7a4f2ea0060
Emon <150001421+doodleEmon@users.noreply.github.com>
2024-11-29 16:13:07 +0600

    get one data and post data is done

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const { connectToDb, getDb } = require('./db');
+const { ObjectId } = require('mongodb');
 
 // init the app && middleware
 const app = express();
+
+app.use(express.json()); // When a client sends a request with a JSON payload (e.g., a POST or PUT request with a JSON body), express.json() parses that JSON data into a JavaScript object and makes it accessible via req.body.
 
 let db;
 
@@ -36,6 +39,34 @@ app.get('/books', (req, res) => {
         })
         .catch(err => {
             console.error('Error fetching data:', err);
-            res.status(500).json({ error: "Could not fetch the data!" });
+            res.status(500).json({ error: "Could not fetch the documents!" });
         });
 });
+
+
+app.get('/books/:id', (req, res) => {
+    if (ObjectId.isValid(req.params.id)) {
+        db.collection('books')
+            .findOne({ _id: new ObjectId(req.params.id) })
+            .then((doc) => {
+                res.status(200).json(doc)
+            })
+            .catch(() => {
+                res.status(500).json({ error: "Could not fetch the document!" })
+            })
+    } else {
+        res.status(500).json({ error: "Could not fetch the document!" })
+    }
+})
+
+app.post('/books', (req, res) => {
+    const book = req.body
+    db.collection('books')
+        .insertOne(book)
+        .then((result) => {
+            res.status(201).json(result)  // 201: successful creation
+        })
+        .catch(() => {
+            res.status(500).json({error: "Could not create the document!"})
+        })
+})
